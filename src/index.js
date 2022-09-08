e26cf9e420829f141c1a9bb0e74ed62fcf83ba80
naanum <nanum@unist.ac.kr>
2022-09-08 17:59:06 +0900

    social Login/out, Nweeting part done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/routes/Home.js
@@ -0,0 +1,43 @@
+import Nweet from "components/Nweet";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+
+const Home = ({ userObj }) => {
+	const [nweet, setNweet] = useState("");
+	const [nweets, setNweets] = useState([]);
+	useEffect(() => {
+		dbService.collection("nweets").onSnapshot((snapshot) => {
+			const nweetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+			setNweets(nweetArray);
+		});
+	}, []);
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		await dbService.collection("nweets").add({
+			text: nweet,
+			createAt: Date.now(),
+			createId: userObj.uid,
+		});
+		setNweet("");
+	};
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+		setNweet(value);
+	};
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+				<input type="submit" value="Nweet" />
+			</form>
+			<div>
+				{nweets.map((nweet) => (
+					<Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.createId === userObj.uid} />
+				))}
+			</div>
+		</div>
+	);
+};
+export default Home;
